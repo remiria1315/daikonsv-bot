51c8adbe2c13e3497e1b1be26b2fbb1113b8159d
remiria1315 <remiria1315@outlook.jp>
2026-06-07 11:55:45 +0900

    initial update

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
